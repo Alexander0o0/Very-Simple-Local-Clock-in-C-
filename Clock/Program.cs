bf5671877798d5e02531d5cdada91dfc862128a5
itloan <itloan@GAMINGLAP05X>
2023-06-29 09:59:21 +0100

    Add project files.

---
+++ Clock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("===================");
+                Console.WriteLine(DateTime.Now);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
